5e39eb9f652458b9223b1e4d98b4578a2b803be0
Débora <deboradiazper@gmail.com>
2022-07-29 07:13:50 +0000

    se mueve registration

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,46 +1,86 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export const Login = () => {
+export const userRegistration = () => {
+  const [data, setData] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (event) => {
+    //console.log(event.target.value);
+    setData({
+      ...data,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const sendData = (event) => {
+    event.preventDefault();
+    console.log(data);
+  };
+
   return (
     <div>
-      <div className="text-center mt-5">
-        <h1>It's nice to see you here!</h1>
+      <div className="container col-12 mb-3 text-center">
+        <h2>Join us!</h2>
+        <p>
+          Join our community and learn about health. It's totally free! Lorem
+          ipsum blablablalblablal
+        </p>
       </div>
-      <form>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            class="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
-          <div id="emailHelp" class="form-text">
-            We'll never share your email with anyone else.
-          </div>
+      <div className="row">
+        <div className="col-12 mb-3 text-center">
+          <form className="registerForm" onSubmit={sendData}>
+            <div className="col-12 mb-3 text-center">
+              <input
+                name="name"
+                placeholder="your name here"
+                autoComplete="off"
+                type="text"
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <div className="col-12 mb-3 text-center">
+              <input
+                name="lastName"
+                placeholder="your last name"
+                autoComplete="off"
+                type="text"
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <div className="col-12 mb-3 text-center">
+              <input
+                name="email"
+                placeholder="your mail"
+                autoComplete="off"
+                type="text"
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <div className="col-12 mb-3 text-center">
+              <input
+                name="password"
+                placeholder="your password"
+                autoComplete="off"
+                type="text"
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <div className="col-12 mb-3 text-center">
+              <button className="btn btn-primary" type="submit">
+                send
+              </button>
+            </div>
+          </form>
         </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-          />
-        </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1">
-            Check me out
-          </label>
-        </div>
-        <button type="submit" class="btn btn-primary">
-          Submit
-        </button>
-      </form>
+      </div>
     </div>
   );
 };
